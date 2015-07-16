f7180e3d28cd95729c42d97ba04aa17eaae513c5
Elaine Matthias <elaine.matthias@gmail.com>
2015-07-15 20:24:30 -0400

    EM's Changes

--- Assignment 4/Class_lab/js/main.js
+++ Assignment 4/Class_lab/js/main.js
@@ -1,15 +1,17 @@
-document.getElementById("buttonOne").onmouseover=changeSize;
-document.getElementById("buttonOne").onmouseout=correctSize;
+document.getElementById("buttonOne").onmouseover = changeSize;
+document.getElementById("buttonOne").onmouseout = correctSize;
 
 //change color of button to yellow and green when hover over.
 function changeSize(){
 	document.getElementById("buttonOne").setAttribute("class","changeSize");
+	console.log("test");
 }
 
 //reset color of button when mouse is off.
 
 function correctSize(){
-	document.getElementById("buttonOne").removeAttribute("class", "changeSize")
+	document.getElementById("buttonOne").removeAttribute("class", "changeSize");
+	console.log('something');
 }
 
 //blue-steel - change font size 
